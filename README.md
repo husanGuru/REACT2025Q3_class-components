9f65c93fc04bcc64701c6a28e31ad78ccbf2afde
husan <abdigafurovhusan@gmail.com>
2025-09-01 05:00:22 +0500

    refactor

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
+# You can use ready json file for faster checks
+
+uncomment necessary lines in src\api\co2.ts
+
 # Initial Profiling with React Dev Tools Profiler
 
 Commit Duration: 0.2s
 Render Duration: 34.8ms
-Interactions: 
-Flame Graph: 
-Ranked Chart: 
+Interactions:
+Flame Graph:
+Ranked Chart:
 
 Initial Load:
 ![alt text](profiler/initial.png)
 Initial Load (ranked):
 ![alt text](profiler/initial_ranked.png)
+
 # Update the App with React.memo and useMemo
 
-Commit Duration: 
-Render Duration: 
-Interactions: 
-Flame Graph: 
-Ranked Chart: 
+Commit Duration:
+Render Duration:
+Interactions:
+Flame Graph:
+Ranked Chart:
--- src/api/co2.ts
+++ src/api/co2.ts
@@ -1,12 +1,18 @@
 import axios from 'axios';
 import { API_URL } from '../utils/const';
-import data from 'src/assets/owid-co2-data.json';
-import { parseCountries } from 'src/utils/country';
 import { CountriesRawData } from 'src/types/country.types';
+import { parseCountries } from 'src/utils/country';
+
+// uncomment for using ready json file
+// import data from 'src/assets/owid-co2-data.json';
 
 export async function getCO2Data(signal?: AbortSignal) {
-  // const data = await axios.get(API_URL, { signal });
+  // comment this line to use json file
+  const response = await axios.get<CountriesRawData>(API_URL, { signal });
 
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  return parseCountries(data as CountriesRawData);
+  // uncomment for using ready json file
+  // return parseCountries(data as CountriesRawData);
+
+  // comment this line to use json file
+  return parseCountries(response.data);
 }
